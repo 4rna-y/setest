7eab32a4e7c8ee2f9f1bd8bbd2aee56fc196820b
ama <ikuyama_kyo@outlook.jp>
2025-09-12 00:24:48 +0900

    fix: lintエラーの解決

--- packages/frontend/tests/App.test.tsx
+++ packages/frontend/tests/App.test.tsx
@@ -1,5 +1,5 @@
 import { render } from "@solidjs/testing-library";
-import { describe, test, expect } from "vitest";
+import { describe, expect, test } from "vitest";
 import App from "../src/App";
 
 describe("App", () => {
