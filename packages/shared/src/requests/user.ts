7455b1b29116a228399418c173c828e507257b01
ama <ikuyama_kyo@outlook.jp>
2025-09-12 18:23:42 +0000

    feat: リクエスト・レスポンスに関する型を作成

---
+++ packages/shared/src/requests/user.ts
@@ -0,0 +1,8 @@
+import z from "zod";
+
+export const CreateUserRequestSchema = z.object({
+    name: z.string(),
+    password: z.string()
+});
+
+export type CreateUserRequest = z.infer<typeof CreateUserRequestSchema>;
